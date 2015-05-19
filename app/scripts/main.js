c4eec8c59276eea73b46c482ae7da8f6b00e6f7d
Tournevis <arthur.ro92@gmail.com>
2015-05-19 13:40:35 +0200

    FINAL

---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+	$('#arrow').click(function(){
+		$.fn.fullpage.moveSectionDown();
+	});
+
+	$('#fullpage').fullpage({
+	        onLeave: function(index, nextIndex, direction){
+	            var leavingSection = $(this);
+
+	            //after leaving section 2
+	            if(index ==1 && direction =='down'){
+	            	//console.log('black now');
+	               $( ".blackoverlay" ).animate({
+					    opacity: 0.4,
+					}, 500);
+	            }
+
+	            else if(index == 2 && direction == 'up'){
+	                $( ".blackoverlay" ).animate({
+					    opacity: 0,
+					}, 500);
+	            }
+	        }
+	    });
+});
